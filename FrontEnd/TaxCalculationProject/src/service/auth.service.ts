094254357e45267613551134e6d9746f16f57397
upendrabhat <bhatupendra1234@gmail.com>
2024-10-23 10:34:04 +0530

    added auth service

---
+++ FrontEnd/TaxCalculationProject/src/service/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  _http : HttpClient;
+  private isAuthenticated = false;
+  private authSecretKey = 'Bearer Token';
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+  });
+
+  constructor(a:HttpClient) {
+    this._http = a;
+    this.isAuthenticated = !!localStorage.getItem(this.authSecretKey) 
+  }
+
+  login(formData:any){
+    this.isAuthenticated = true;
+  }
+
+  logout(): void{
+    localStorage.removeItem(this.authSecretKey);
+    this.isAuthenticated = false;
+  }
+
+  isAuthenticatedUser(): boolean {
+    return this.isAuthenticated;
+  }
+}
